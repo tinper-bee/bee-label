da4d7dbad1d6cec51eeb78b20e7145437e381f5c
ahua52 <1468492018@qq.com>
2016-10-20 15:04:38 +0800

    ver. 0.0.2

--- build/bee-control-label.js
+++ build/bee-control-label.js
@@ -64,3 +64,17 @@ var ControlLabel = function (_Component) {
 
 exports["default"] = ControlLabel;
 module.exports = exports['default'];
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _ControlLabel = require('./ControlLabel');
+
+var _ControlLabel2 = _interopRequireDefault(_ControlLabel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports["default"] = _ControlLabel2["default"];
+module.exports = exports['default'];
