7eae18b7966f173cf5f2e0a6d7eee5fbaeac236d
“liurhc” <liurhc@yonyou.com>
2016-11-02 14:57:56 +0800

    新增例子

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,14 @@
-import Demo from './ControlLabelDemo';
+import ControlLabel from '../src';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<Demo/>, document.getElementById('tinperBeeDemo'));
+
+
+function demo1() {
+    return (
+        <div className="demo-control-label">
+            <ControlLabel>control label</ControlLabel> 
+        </div>
+    );
+}
+
+ReactDOM.render(demo1(), document.getElementById('ReactControlLabelDemo1'));
