75c91917f37783d57ffb700c1989d7115ea38484
huyueb <1062887235@qq.com>
2018-01-16 13:22:13 +0800

    修改demo展现方式

--- demo/index.js
+++ demo/index.js
@@ -4,7 +4,6 @@ import { Panel } from 'bee-panel';
 import Button from 'bee-button';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Label from '../src';
 
 
 const CARET = <i className="uf uf-arrow-down"></i>;
@@ -12,7 +11,7 @@ const CARET = <i className="uf uf-arrow-down"></i>;
 const CARETUP = <i className="uf uf-arrow-up"></i>;
 
 
-var Demo1 = require("./demolist/Demo1");var DemoArray = [{"example":<Demo1 />,"title":"Demo1","code":"/**\n * \n * \n */\n\nimport React, { Component } from 'react';\nimport Label from 'bee-label';\n\n\trender () {\n\t\treturn (\n\t\t\t<Label>control label</Label> \n\t\t)\n\t}\n}","desc":""}]
+var Demo1 = require("./demolist/Demo1");var DemoArray = [{"example":<Demo1 />,"title":"Demo1","code":"/**\n * \n * \n */\n\nimport React, { Component } from 'react';\nimport { Label } from 'tinper-bee';\n\n\trender () {\n\t\treturn (\n\t\t\t<Label>control label</Label> \n\t\t)\n\t}\n}","desc":""}]
 
 
 class Demo extends Component {
@@ -28,22 +27,26 @@ class Demo extends Component {
     }
 
     render () {
-        const { title, example, code, desc  } = this.props;
+        const { title, example, code, desc, scss_code  } = this.props;
         let caret = this.state.open ? CARETUP : CARET;
         let text = this.state.open ? "隐藏代码" : "查看代码";
 
-        const footer = (
-            <Button shape="block" onClick={ this.handleClick }>
-                { caret }
-                { text }
-            </Button>
+        const header = (
+            <div>
+                {example}
+                <Button style={{"marginTop": "10px"}} shape="block" onClick={ this.handleClick }>
+                    { caret }
+                    { text }
+                </Button>
+            </div>
         );
         return (
-            <Col md={12}>
+            <Col md={12} >
                 <h3>{ title }</h3>
                 <p>{ desc }</p>
-                <Panel collapsible expanded={ this.state.open } colors='bordered' header={ example } footer={footer} footerStyle = {{padding: 0,borderColor: "transparent"}} >
+                <Panel collapsible headerContent expanded={ this.state.open } colors='bordered' header={ header } footerStyle = {{padding: 0}}>
                     <pre><code className="hljs javascript">{ code }</code></pre>
+                    { !!scss_code ? <pre><code className="hljs css">{ scss_code }</code></pre> : null }
                 </Panel>
             </Col>
         )
@@ -60,7 +63,7 @@ class DemoGroup extends Component {
                     {DemoArray.map((child,index) => {
 
                         return (
-                            <Demo example= {child.example} title= {child.title} code= {child.code} desc= {child.desc} key= {index}/>
+                            <Demo example= {child.example} title= {child.title} code= {child.code} scss_code= {child.scss_code} desc= {child.desc} key= {index}/>
                         )
 
                     })}
